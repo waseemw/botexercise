f1dcf6c7586599652ab7ff9c6247ffd702b4aa05
Waseem <waseem.wasaya@gmail.com>
2018-12-30 16:27:08 +0300

    First working version

--- components/getshipmentinfo.js
+++ components/getshipmentinfo.js
@@ -2,27 +2,27 @@
 var request = require('request');
 module.exports = {
     metadata: () => ({
-    name: 'ShipmentChecker',
-    properties: {
-        id: {
-            required: true,
-            type: 'string'
+        name: 'ShipmentChecker',
+        properties: {
+            id: {
+                required: true,
+                type: 'string'
+            },
         },
-    },
-    supportedActions: ["success", "error"]
-}),
+        supportedActions: ["success", "error"]
+    }),
     invoke: (conversation, done) => {
-        const { id } = conversation.properties();
-        const url='https://atapi2.postnord.com/rest/shipment/v1/trackandtrace/findByIdentifier.json?apikey=3c148d4769d841823f6942470675b2da&id='+id+'&locale=en';
-        request(url, function(error, res, body){
-            var json = JSON.parse(body);
-            var arrivalDate = json.TrackingInformationResponse.shipments.estimatedTimeOfArrival;
-            if (arrivalDate) {
-                let date = new Date(arrivalDate);
-                conversation.variable("date",date.getDate());
-                conversation.variable("time",date.getTime());
+        const {id} = conversation.properties();
+        const url = 'https://atapi2.postnord.com/rest/shipment/v1/trackandtrace/findByIdentifier.json?apikey=9f7708e8b2a1b519946c7cdebff6a0c5&id=' + id + '&locale=en';
+        request(url, function (error, res, body) {
+            let json = JSON.parse(body);
+            let shipmentInfo = json.TrackingInformationResponse.shipments[0];
+            if (shipmentInfo) {
+                let date = new Date(shipmentInfo.deliveryDate);
+                conversation.variable("date", date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear());
+                conversation.variable("time", date.getHours() + ':' + date.getMinutes());
                 conversation.transition("success");
-            }else{
+            } else {
                 conversation.reply("Seems like you entered an invalid shipment id, try again...").transition("error");
             }
             done();
